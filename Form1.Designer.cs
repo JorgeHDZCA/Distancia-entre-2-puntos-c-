b4ef47f6c927b027ccadabb56a8df0b18544e32f
JorgeHDZCA <123972008+JorgeHDZCA@users.noreply.github.com>
2023-01-30 09:50:47 -0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace distancia_entre_2_puntos_c_char
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.X1 = new System.Windows.Forms.TextBox();
+            this.Y1 = new System.Windows.Forms.TextBox();
+            this.Y2 = new System.Windows.Forms.TextBox();
+            this.X2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Dis = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // X1
+            // 
+            this.X1.Location = new System.Drawing.Point(290, 400);
+            this.X1.Name = "X1";
+            this.X1.Size = new System.Drawing.Size(100, 22);
+            this.X1.TabIndex = 0;
+            // 
+            // Y1
+            // 
+            this.Y1.Location = new System.Drawing.Point(139, 294);
+            this.Y1.Name = "Y1";
+            this.Y1.Size = new System.Drawing.Size(100, 22);
+            this.Y1.TabIndex = 1;
+            // 
+            // Y2
+            // 
+            this.Y2.Location = new System.Drawing.Point(139, 137);
+            this.Y2.Name = "Y2";
+            this.Y2.Size = new System.Drawing.Size(100, 22);
+            this.Y2.TabIndex = 2;
+            // 
+            // X2
+            // 
+            this.X2.Location = new System.Drawing.Point(532, 398);
+            this.X2.Name = "X2";
+            this.X2.Size = new System.Drawing.Size(100, 22);
+            this.X2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(323, 425);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "X1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(569, 425);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "X2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(110, 297);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Y1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(110, 143);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Y2";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 379);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 43);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Realizar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Dis
+            // 
+            this.Dis.AutoSize = true;
+            this.Dis.Location = new System.Drawing.Point(58, 444);
+            this.Dis.Name = "Dis";
+            this.Dis.Size = new System.Drawing.Size(17, 16);
+            this.Dis.TabIndex = 9;
+            this.Dis.Text = "R";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(245, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(445, 366);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(730, 491);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Dis);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.X2);
+            this.Controls.Add(this.Y2);
+            this.Controls.Add(this.Y1);
+            this.Controls.Add(this.X1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox X1;
+        private System.Windows.Forms.TextBox Y1;
+        private System.Windows.Forms.TextBox Y2;
+        private System.Windows.Forms.TextBox X2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label Dis;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace distancia_entre_2_puntos_c_char
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double d = 0;
+            d = Math.Sqrt(Math.Pow(double.Parse(X1.Text) - double.Parse(X2.Text), 2) + Math.Pow(double.Parse(Y1.Text) - double.Parse(Y2.Text), 2));
+            Dis.Text = d.ToString();
+        }
+    }
+}
